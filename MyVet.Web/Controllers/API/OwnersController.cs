e42d23f713eb1685bbade44eabe2405deff5fb3f
stanlyh <eduardostanly@gmail.com>
2019-09-21 21:42:51 -0500

    add Token

--- MyVet.Web/Controllers/API/OwnersController.cs
+++ MyVet.Web/Controllers/API/OwnersController.cs
@@ -11,7 +11,7 @@ namespace MyVet.Web.Controllers.API
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class OwnersController : ControllerBase
     {
         private readonly DataContext _dataContext;
